08215b91aee48dc2cad6d9817be28349c242a3a2
andrei-marinica <andrei_m_marinica@yahoo.com>
2020-03-18 15:33:36 +0200

    numeric types arg, result and storage tests

--- features/src/lib.rs
+++ features/src/lib.rs
@@ -4,6 +4,8 @@
 #![allow(non_snake_case)]
 #![allow(unused_attributes)]
 
+static ZERO_KEY: [u8; 32] = [0u8; 32];
+
 #[elrond_wasm_derive::contract(ApiFeatureExamplesImpl)]
 pub trait ApiFeatureExamples {
 
@@ -14,9 +16,60 @@ pub trait ApiFeatureExamples {
         panic!("example panic message");
     }
 
-    fn variableLengthArg(&self, arg: Vec<u8>) -> (Vec<u8>, i64) {
+    fn echo_big_uint(&self, bi: BigUint) -> BigUint {
+        bi
+    }
+
+    fn store_big_uint(&self, bi: BigUint) {
+        self.storage_store_big_uint(&ZERO_KEY.into(), &bi);
+    }
+
+    fn load_big_uint(&self) -> BigUint {
+        self.storage_load_big_uint(&ZERO_KEY.into())
+    }
+
+    fn echo_big_int(&self, bi: BigInt) -> BigInt {
+        bi
+    }
+
+    fn store_big_int(&self, bi: BigInt) {
+        self.storage_store_big_int(&ZERO_KEY.into(), &bi);
+    }
+
+    fn load_big_int(&self) -> BigInt {
+        self.storage_load_big_int(&ZERO_KEY.into())
+    }
+
+    fn echo_i64(&self, i: i64) -> i64 {
+        i
+    }
+
+    fn store_i64(&self, i: i64) {
+        self.storage_store_i64(&ZERO_KEY.into(), i);
+    }
+
+    fn load_i64(&self) -> (bool, i64) {
+        let o = self.storage_load_i64(&ZERO_KEY.into());
+        match o {
+            Some(i) => (true, i),
+            None => (false, 0)
+        }
+    }
+
+    fn echo_vec_u8(&self, arg: Vec<u8>) -> (Vec<u8>, i64) {
+        self.storage_store(&ZERO_KEY.into(), &arg);
         let l = arg.len() as i64;
         (arg, l)
+    }
+
+    fn store_vec_u8(&self, arg: Vec<u8>) {
+        self.storage_store(&ZERO_KEY.into(), &arg)
+    }
+
+    fn load_vec_u8(&self) -> (Vec<u8>, i64) {
+        let v = self.storage_load(&ZERO_KEY.into());
+        let l = v.len() as i64;
+        (v, l)
     }
 
     fn storageLoadVec(&self) -> (Vec<u8>, i64) {
