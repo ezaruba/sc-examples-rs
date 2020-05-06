b8dc626b37b2e830b197463e86f76189fc902d23
andrei-marinica <andrei.marinica@elrond.com>
2020-05-06 14:36:27 +0300

    crypto bubbles var length keys update

--- crypto-bubbles/src/lib.rs
+++ crypto-bubbles/src/lib.rs
@@ -4,7 +4,7 @@
 #![allow(non_snake_case)]
 #![allow(unused_attributes)]
 
-static OWNER_KEY: [u8; 32] = [0u8; 32];
+static OWNER_KEY: &[u8] = &[0u8; 32];
 
 #[elrond_wasm_derive::contract(CryptoBubblesImpl)]
 pub trait CryptoBubbles
@@ -14,7 +14,7 @@ pub trait CryptoBubbles
     /// will set the fixed global token supply and give all the supply to the creator
     fn init(&self) {
         let sender = self.get_caller();
-        self.storage_store_bytes32(&OWNER_KEY.into(), &sender.into());
+        self.storage_store_bytes32(OWNER_KEY, &sender.into());
     }
 
     /// generates the balance key that maps balances with their owners
@@ -30,7 +30,7 @@ pub trait CryptoBubbles
     /// getter function: retrieves balance for an account
     fn balanceOf(&self, subject: Address) -> BigUint {
         let balance_key = self._player_balance_key(&subject);
-        let balance = self.storage_load_big_uint(&balance_key);
+        let balance = self.storage_load_big_uint(balance_key.as_bytes());
         balance
     }
 
@@ -40,9 +40,9 @@ pub trait CryptoBubbles
         let caller = self.get_caller();
         
         let balance_key = self._player_balance_key(&caller);
-        let mut balance = self.storage_load_big_uint(&balance_key);
+        let mut balance = self.storage_load_big_uint(balance_key.as_bytes());
         balance += &payment;
-        self.storage_store_big_uint(&balance_key, &balance);
+        self.storage_store_big_uint(balance_key.as_bytes(), &balance);
 
         self.top_up_event(&caller, &payment);
     }
@@ -56,12 +56,12 @@ pub trait CryptoBubbles
     #[private]
     fn _transferBackToPlayerWallet(&self, player: &Address, amount: &BigUint) -> Result<(), &str> {
         let balance_key = self._player_balance_key(&player);
-        let mut balance = self.storage_load_big_uint(&balance_key);
+        let mut balance = self.storage_load_big_uint(balance_key.as_bytes());
         if amount > &balance {
             return Err("amount to withdraw must be less or equal to balance");
         }
         balance -= amount;
-        self.storage_store_big_uint(&balance_key, &balance);
+        self.storage_store_big_uint(balance_key.as_bytes(), &balance);
 
         self.send_tx(player, &amount, "crypto bubbles");
 
@@ -74,12 +74,12 @@ pub trait CryptoBubbles
     #[private]
     fn _addPlayerToGameStateChange(&self, game_index: &BigUint, player: &Address, bet: &BigUint) -> Result<(), &str> {
         let balance_key = self._player_balance_key(&player);
-        let mut balance = self.storage_load_big_uint(&balance_key);
+        let mut balance = self.storage_load_big_uint(balance_key.as_bytes());
         if bet > &balance {
             return Err("insufficient funds to join game");
         }
         balance -= bet;
-        self.storage_store_big_uint(&balance_key, &balance);
+        self.storage_store_big_uint(balance_key.as_bytes(), &balance);
 
         self.player_joins_game_event(game_index, player, bet);
 
@@ -99,15 +99,15 @@ pub trait CryptoBubbles
     // owner transfers prize into winner SC account
     fn rewardWinner(&self, game_index: &BigUint, winner: &Address, prize: &BigUint) -> Result<(), &str> {
         let caller = self.get_caller();
-        let owner: Address = self.storage_load_bytes32(&OWNER_KEY.into()).into();
+        let owner: Address = self.storage_load_bytes32(OWNER_KEY).into();
         if caller != owner {
             return Err("invalid sender: only contract owner can reward winner");
         }
 
         let balance_key = self._player_balance_key(&winner);
-        let mut balance = self.storage_load_big_uint(&balance_key);
+        let mut balance = self.storage_load_big_uint(balance_key.as_bytes());
         balance += prize;
-        self.storage_store_big_uint(&balance_key, &balance);
+        self.storage_store_big_uint(balance_key.as_bytes(), &balance);
 
         self.reward_winner_event(game_index, &winner, &prize);
 
